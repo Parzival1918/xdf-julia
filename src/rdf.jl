db0f4510d52c4373828bc282c976b0f2a76b350b
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2023-12-10 10:22:45 +0000

    first commit

---
+++ src/rdf.jl
@@ -0,0 +1,11 @@
+# RDF Algorithm adapted from book: 
+#       Understanding Molecular Simulation, From Algorithms to Applications 3rd edition
+
+# Functions 
+
+
+# Vars
+
+nhis = 100 # number of bins
+
+#delg = box/(2*nhis) # bin size
